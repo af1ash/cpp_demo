ba5a857f5cb6570bca785870b32e25bcd13dbe60
lixiaobing <li_120603@163.com>
2025-07-27 18:47:52 +0800

    [feat] 使用cmake tools 构建

--- src/process/process.cpp
+++ src/process/process.cpp
@@ -1,20 +1,20 @@
 #include<iostream>
 #include "process.h"
-#include <Eigen/Dense>
+// #include <Eigen/Dense>
 
 using std::cout;
 using std::endl;
-using Eigen::MatrixXd;
+// using Eigen::MatrixXd;
 
 void Process::planProcess()
 {
     cout << "this is planing process" << endl;
     // my_map.mapInfo();
-    MatrixXd m(2, 2);
-    m(0, 0) = 3;
-    m(1, 0) = 5;
-    m(0, 1) = -1;
-    m(1, 1) = m(1, 0) + m(0, 1);
-    cout << m << endl;
+    // MatrixXd m(2, 2);
+    // m(0, 0) = 3;
+    // m(1, 0) = 5;
+    // m(0, 1) = -1;
+    // m(1, 1) = m(1, 0) + m(0, 1);
+    // cout << m << endl;
     cout << "planing success" << endl;
 }
